a3970110d3b45f5183aceb919ef14fc16c87f912
Jana Hamadi <jana.f.hamadi@gmail.com>
2021-04-29 06:48:18 +0200

    project init

---
+++ api/src/test/java/com/testwebflux/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testwebflux.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
